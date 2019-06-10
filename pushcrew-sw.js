30e63721177a429e5382be1cc60e2e445e1fa7f5
Gaurav Nanda <93.gaurav@gmail.com>
2019-06-10 16:26:35 +0530

    Use PushCrew

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrewstaging.com/sw/2de4d2c5d907bd0e1e697052f83c575b.js');
+importScripts('https://cdn.pushcrew.com/sw/c746a6786890c4e74cb25498556803fc.js');
