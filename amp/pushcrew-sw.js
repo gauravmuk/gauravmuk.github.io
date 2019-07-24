d2e812ea715557aec5d49d52c21acac838ef4c08
Gaurav Nanda <93.gaurav@gmail.com>
2019-07-24 13:18:09 +0530

    Use VAPID properly

--- amp/pushcrew-sw.js
+++ amp/pushcrew-sw.js
@@ -175,8 +175,8 @@ function broadcastReply(command, payload) {
 
 function urlBase64ToUint8Array(base64String) {
     var padding = '='.repeat((4 - base64String.length % 4) % 4);
-    var base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
-    var rawData = window.atob(base64);
+    var base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
+    var rawData = atob(base64);
     var outputArray = new Uint8Array(rawData.length);
     for (var i = 0; i < rawData.length; ++i) {
         outputArray[i] = rawData.charCodeAt(i);
