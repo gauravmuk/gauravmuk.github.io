57fdc6c1b9c90608ac45af1612310e782419e2ce
Gaurav Nanda <93.gaurav@gmail.com>
2020-05-18 13:29:29 +0530

    Move to newer place

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 8;
-importScripts('https://cdn.pushcrew.com/sw/041f7f314f22c3a7cbcd81d84fd0e6d5.js');
+importScripts('https://cdn.pushcrew.com/sw/238fb34554ae3f8c70d499d26529b594.js');
