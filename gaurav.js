c4d387b12a9d5b9a5fba983396e20789b981f184
Gaurav Nanda <93.gaurav@gmail.com>
2020-05-27 16:36:40 +0530

    Staging Env

--- gaurav.js
+++
@@ -1,2 +0,0 @@
-var version = 8;
-importScripts('https://cdn.pushcrew.com/sw/238fb34554ae3f8c70d499d26529b594.js');
---
+++ pushcrew-sw.js
@@ -0,0 +1,2 @@
+var version = 10;
+importScripts('https://cdn1.pushcrew.com/sw/51f0062c4acb3fa0dc93fc0bdafae1e8.js');
