d936a1eea86baab4d0175305c2307a0dcaab096e
Gaurav Nanda <93.gaurav@gmail.com>
2020-09-18 13:34:29 +0530

    Trying different

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -2,6 +2,6 @@ var version = 12;
 if (location.hostname === 'localhost') {
     importScripts(`https://cdn.pushcrew.com/sw/c746a6786890c4e74cb25498556803fc.js`);
 } else {
-    importScripts('https://cdn.pushcrew.com/sw/340dd5bbb930dfb5357a3220c40f3127.js');
+    importScripts('https://cdn.pushcrew.com/sw/e0a4b04cf201feb52c477318c588494b.js');
 }
 
