44308fd209025a4a0a5462a486a3e8086ecd5ad2
Gaurav Nanda <93.gaurav@gmail.com>
2020-09-18 13:21:29 +0530

    Trying conditional imports

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,7 @@
 var version = 12;
-importScripts('https://cdn.pushcrew.com/sw/c746a6786890c4e74cb25498556803fc.js');
+if (location.hostname === 'localhost') {
+    importScripts(`https://cdn.pushcrew.com/sw/c746a6786890c4e74cb25498556803fc.js`);
+} else {
+    importScripts('https://cdn.pushcrew.com/sw/340dd5bbb930dfb5357a3220c40f3127.js');
+}
+
